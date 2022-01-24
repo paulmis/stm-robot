6c62d05b302c33d539e048aff492f6bd2501a45d
paulmis <chugdevs@gmail.com>
2022-01-24 21:12:45 +0100

    separated fixed-distance travel

--- src/platoon.ino
+++ src/platoon.ino
@@ -2,24 +2,14 @@ extern int frontDistance;
 
 void platoon(int threshold) {
   ultrasonicFront();
-  Serial.print("frontDistance: ");
-  Serial.println(frontDistance);
-
+  
   // Get the IFR readouts and check if thresholds were broken
   int l = ifr(IFR_L), r = ifr(IFR_R);
   bool cl = l > threshold, cr = r > threshold;
 
-  Serial.print("Left: ");
-  Serial.print(l);
-  Serial.print(", Right: ");
-  Serial.println(r);
-  
-  if (frontDistance > 60 || (frontDistance >= 12 && frontDistance < 14)) {
+  if (frontDistance > 60 || (frontDistance >= 12 && frontDistance < 14))
     freeze();
-    Serial.println("STOP");
-  }
   else if (frontDistance < 12) {
-    Serial.println("BACKWARDS speed: 200");
     if (cl ^ cr) {
       if (cl) {
         mtr(155, 0, 220, 0);
@@ -35,9 +25,6 @@ void platoon(int threshold) {
   }
   else {
     int speed = max(0, min(200, 15 * frontDistance - 70));
-    Serial.print("FOWARDS speed: ");
-    Serial.println(speed);
-  
     if (cl ^ cr) {
       if (cl) leftonly(180);
       else    rightonly(180);
